affbef933260df2d890456888a245f8d7b52ff71
wenodh <vinodkumara3@gmail.com>
2021-10-24 02:55:47 +0530

    login and register pages added

--- react-social/src/pages/login/Login.jsx
+++ react-social/src/pages/login/Login.jsx
@@ -9,7 +9,15 @@ export default function Login() {
                     <span className="loginDesc">Connect with friends.</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox"></div>
+                    <div className="loginBox">
+                        <input placeHolder="Email" className="loginInput" />
+                        <input placeHolder="Password" className="loginInput" />
+                        <button className="loginButton">Login In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">
+                            Create a New Account
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
