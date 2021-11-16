5a1202ec58ae48b8204eecde1f8e16e996e9266e
wenodh <vinodkumara3@gmail.com>
2021-11-17 00:45:20 +0530

    socket server added

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,44 @@
 const io = require('socket.io')(8900, {
     cors: {
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
     },
 });
 
+let users = [];
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId });
+};
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
 io.on('connection', (socket) => {
+    //when connect
     console.log('a user connected.');
-    io.emit('message', 'hello this socket server');
+
+    //take userId and socketId from user
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id);
+        console.log(userId, socket.id);
+        console.log(users);
+    });
+
+    //send and get message
+    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit('getMessage', {
+            senderId,
+            text,
+        });
+    });
+
+    //when disconnect
+    socket.on('disconnect', () => {
+        removeUser(socket.id);
+        console.log('a user disconnected.');
+        io.emit('getUsers', users);
+    });
 });
